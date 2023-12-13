812446ac967fb54b40468d27503ec0d86cbbdcda
Artlantic <1282888673jx@gmail.com>
2023-12-13 17:34:31 +0800

    Update test_helper_demo.cpp

--- test_helper_demo.cpp
+++ test_helper_demo.cpp
@@ -1,27 +1,48 @@
 #include "test_helper.h"
+#include <functional>
 using namespace test_helper;
 
-int func(char a, std::string b) {
-    std::cout << "print:" << a << " " << b << " end.\n";
-    return 1;
+int func(const char& a, const std::string& b) {
+    std::cout << "print0:" << a << " " << b << " end.\n";
+    return 0;
 }
 int func1(int a, std::string b, std::string c) {
-    std::cout << "print:" << a << " " << b << " " << c << " end.\n";
-    return 2;
+    std::cout << "print1:" << a << " " << b << " " << c << " end.\n";
+    return 1;
 }
 
-TEST_INIT_BEGIN
-ADD_TEST_FUNCTION("echo", func)
-ADD_TEST_FUNCTION("echo1", func1)
-TEST_INIT_END
+class c {
+public:
+    static int func2(int a) {
+        std::cout << "print2:" << a << " end.\n";
+        return 3;
+    }
+    int func3(int a) {
+        std::cout << "print3:" << a << " end.\n";
+        return 2;
+    }
+};
+
+
+void test(const std::string& name_, const std::vector<std::string>& arg_, std::string& result_) {
+    TEST_INIT(name_, arg_, result_)
+    c ci;
+    auto bind3                    = std::bind(&c::func3, &ci, std::placeholders::_1);
+    std::function<int(int)> func3 = bind3;
+    ADD_TEST_STD_FUNCTION("echo3", func3)
+
+    ADD_TEST_FUNCTION("echo", func)
+    ADD_TEST_FUNCTION("echo1", func1)
+    ADD_TEST_FUNCTION("echo2", c::func2)
+}
 
 int main() {
     std::vector<std::string> req;
     std::string res;
-    req = {"1", "str"};
-    CALL_TEST("echo", req, res)
+    req = {"c", "str"};
+    test("echo", req, res);
     req = {"3", "str2", "123"};
-    CALL_TEST("echo1", req, res)
-    req = {"ff", "str1", ""};
-    CALL_TEST("echo1", req, res)
+    test("echo1", req, res);
+    req = {"1ff", "str1", ""};
+    test("echo3", req, res);
 }
